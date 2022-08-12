88ab797aa39c782dbf0cca83129d133aac71677c
Andrea Chierici <chierici@cnaf.infn.it>
2022-08-12 09:29:00 +0000

    Update attaching_a_volume.sh
    Update the exercise to reflect recent changes

--- attaching_a_volume.sh
+++ attaching_a_volume.sh
@@ -1,34 +1,33 @@
-df -h             #check the mounted filesystem
-fdisk -l          #list the attached disks
-fdisk /dev/sdb    #use your disk label obtained by the previous command
-
-######## PAY ATTENTION AND AVOID FORMATTING THE SYSTEM DISK!!!!!!!!!!
-# now you have an interactive interface to create a new partition in the disk, use teh commands p, n, p, w keep the defaults
-# fdisk commands: p, n (primary partition and use the defaults), p, w
-
-mkfs.ext4 /dev/sdb1   # create the ext4 filesystem
-mkdir /data2          # create a mountpoint for the new filesystem
-yum install vim       # if not present
-vim /etc/fstab        # edit the fstab file
-# add this line at the end of the fstab file:    /dev/sdb1     /data2  ext4 defaults 0 0
+###############################
+# Inspect the storage on VM
+sudo -if		# we neet do be root@ip-172-31-12-71
+df -h           #check the mounted filesystem
+lsblk          	#list the attached disks
 
 ###############################
-#Inspect the fstab file
-[root@ip-172-31-12-71 ~]# cat /etc/fstab
+# We partition the new disk
+parted --script /dev/sdb mklabel msdos mkpart primary ext4 1MiB 10GB
+lsblk			# you should now see sdb1 below sdb disk, with approx 9GB
 
-#
-# /etc/fstab
-# Created by anaconda on Mon Jan 28 15:24:25 2019
-#
-# Accessible filesystems, by reference, are maintained under '/dev/disk'
-# See man pages fstab(5), findfs(8), mount(8) and/or blkid(8) for more info
-#
-UUID=4a1c93d9-eb47-4f96-9f3d-920e52dc8cca /                       xfs     defaults        0 0
-/dev/sdb1     /data2  ext4 defaults 0 0
-##########################################################
+mkfs.ext4 /dev/sdb1   	# create the ext4 filesystem
+mkdir /data2          	# create a mountpoint for the new filesystem
+yum -y install nano       	# if not present
+lsblk -o name,uuid		# identify sdb1 uuid
+nano /etc/fstab        	# edit the fstab file
+# add this line at the end of the fstab file:    
+UUID=<the uuid associated to sdb1>     /data2  ext4 defaults 0 0
 
+###############################
+#Verify the fstab file
+cat /etc/fstab
+
+# it should contain the line regarding data2 mount point
+
+###############################
+# We now mount the new filesystem on /data2
 df -h
-mount -a    #mount all the filesystem listed in the fstab file
-df -h   #check the differences
+mount -a    			# mount all the filesystem listed in the fstab file
+df -h   				# check the differences
+touch /data2/foobar
 ll /data2
 chmod 775 /data2/
