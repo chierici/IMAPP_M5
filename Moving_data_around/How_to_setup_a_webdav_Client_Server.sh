4db96522ac0a33c69c3046abde85cc30fd92dca1
Andrea Chierici <chierici@cnaf.infn.it>
2023-10-29 15:46:52 +0100

    Update How_to_setup_a_webdav_Client_Server.sh

--- Moving_data_around/How_to_setup_a_webdav_Client_Server.sh
+++ Moving_data_around/How_to_setup_a_webdav_Client_Server.sh
@@ -72,6 +72,7 @@ semanage fcontext -a -t httpd_sys_rw_content_t /var/www/html/webdav
 
 yum -y install cadaver
 cadaver http://<your-server-ip>/webdav/
+touch pippo pluto
 
 # To upload a file in your home dir, like "/home/user/abc.txt" to the WebDAV server:
 
