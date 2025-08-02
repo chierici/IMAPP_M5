90b9fac13cb0cc44d657e437ab56ead483036ef3
Andrea Chierici <chierici@cnaf.infn.it>
2025-08-02 19:55:18 +0200

    Update attaching_a_volume.sh

--- attaching_a_volume.sh
+++ attaching_a_volume.sh
@@ -1,12 +1,12 @@
 ###############################
 # Inspect the storage on VM
-sudo -i         # we need do be root
+sudo -i         # we need to be root
 df -h           # check the mounted filesystem
 lsblk           # list the attached disks
 
 ###############################
 # Now add the disk from the vm web interface, specify 10GB size.
-# Issue again this command to see sdb appear in the list of disks:
+# Issue again this command to see "sdb" appear in the list of disks:
 
 lsblk
 
@@ -18,10 +18,10 @@ lsblk                   # you should now see sdb1 below sdb disk, with approx 9G
 
 mkfs.ext4 /dev/sdb1     # create the ext4 filesystem
 mkdir /data2            # create a mountpoint for the new filesystem
-yum -y install nano     # if not present
+yum -y install nano     # if not present, install nano editor
 lsblk -o name,uuid      # identify sdb1 uuid
 nano /etc/fstab         # edit the fstab file
-# add this line at the end of the fstab file:    
+# add this line at the end of the fstab file (copy the UUID with the mouse):    
 UUID=<the uuid associated to sdb1>     /data2  ext4 defaults 0 0
 
 ###############################
@@ -39,3 +39,4 @@ df -h                   # check the differences
 touch /data2/foobar
 ll /data2
 chmod 775 /data2/
+
