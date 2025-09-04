19faddf3bcfe708ccf0a48320ea88f0b9fa3f1c2
Andrea Chierici <chierici@cnaf.infn.it>
2025-09-04 16:42:51 +0200

    Update attaching_a_volume.sh

--- attaching_a_volume.sh
+++ attaching_a_volume.sh
@@ -5,16 +5,10 @@ df -h           # check the mounted filesystem
 lsblk           # list the attached disks
 
 ###############################
-# Now add the disk from the vm web interface, specify 10GB size.
-# Issue again this command to see "sdb" appear in the list of disks:
-
-lsblk
-
-###############################
 # We partition the new disk
-parted --script /dev/sdb mklabel msdos mkpart primary ext4 1MiB 10GB
+parted --script /dev/sdb mklabel msdos mkpart primary ext4 1MiB 100GB
 
-lsblk                   # you should now see sdb1 below sdb disk, with approx 9GB
+lsblk                   # you should now see sdb1 below sdb disk, with approx 93GB
 
 mkfs.ext4 /dev/sdb1     # create the ext4 filesystem
 mkdir /data2            # create a mountpoint for the new filesystem
@@ -38,5 +32,6 @@ systemctl daemon-reload
 df -h                   # check the differences
 touch /data2/foobar
 ll /data2
-chmod 775 /data2/
+
+
 
