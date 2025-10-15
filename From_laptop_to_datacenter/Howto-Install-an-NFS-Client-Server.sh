28572470e82824660c7ecc08f5e202e900722f20
Andrea Chierici <chierici@cnaf.infn.it>
2025-10-15 21:48:27 +0200

    Update Howto-Install-an-NFS-Client-Server.sh

--- From_laptop_to_datacenter/Howto-Install-an-NFS-Client-Server.sh
+++ From_laptop_to_datacenter/Howto-Install-an-NFS-Client-Server.sh
@@ -1,15 +1,19 @@
+#####################################################################
+# Install 2 virtual machine, one called server, and one called client
+# when the machines are ready, procede with the following steps
+
 ####################
 # On the server
 
 # Commands to be executed by root user
-sudo su -
+sudo -i
 mkdir /data
 chmod 755 /data
 touch /data/nfs-test
 yum -y install nfs-utils nano
 
 nano /etc/exports
-      /data  <destination_host IP - USE the private IP>(rw,sync,no_wdelay)
+      /data  <destination_host IP - USE the private IP of client machine>(rw,sync,no_wdelay)
 
 cat /etc/exports  # verify your editing
 
@@ -29,14 +33,21 @@ firewall-cmd --reload
 # On the client
 
 # Commands to be executed by root user 
+sudo -i
 mkdir /data_mounted
 yum -y install nfs-utils
-mount -t nfs <your_server_ip>:/data /data_mounted
+mount -t nfs <your_server_private_ip>:/data /data_mounted
+df -h
 ll /data_mounted/
 cat /etc/mtab | grep data_mounted
+
+# now you can play creating files on server and checking if they appear on client
+
+# after the previous steps, you can unmount the file system
 umount /data_mounted
 
 # in case we want to automount the FS at boot time
 nano /etc/fstab
 # add the following line at the end of the file
 <SERVER_PRIVATE_IP>:/data /data_mounted   nfs defaults        0 0
+
